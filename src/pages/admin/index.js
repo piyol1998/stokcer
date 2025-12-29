1bb23aec253ca5e341698c2b8c52c9165edbe667
Sofyan Alfandi <sofyanlfandy@gmail.com>
2025-12-29 20:24:09 +0300

    Remove admin pages directory

--- src/pages/admin/index.js
+++
@@ -1,5 +0,0 @@
-
-export { default as AdminLogin } from './AdminLogin';
-export { default as WebDevConsole } from './WebDevConsole';
-export { default as AdminDashboard } from './AdminDashboard';
-export { default as CreateOwnerModal } from './CreateOwnerModal';
